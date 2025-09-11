b0672b3eb33e63c68d456b420fdd1e99fc383c74
Om Patki <om.y.patki@gmail.com>
2025-09-11 12:34:17 -0700

    fixed header link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,20 +5,22 @@ import Link from 'next/link'
 
 export default function Header() {
     return (
-        <Link href="/">
-            <header className="p-4 bg-[#9A3F3F] flex justify-between">
-                <Logo />
-                <SignedOut>
-                    <SignInButton>
-                        <button type="button" className="text-white px-4 py-2 cursor-pointer rounded hover:bg-[#5A0808]">
-                            Sign in
-                        </button>
-                    </SignInButton>
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-            </header>
-        </Link>
+        <header className="p-4 bg-[#9A3F3F] flex justify-between">
+            <div className="flex items-center gap-10">
+                <Link href="/">
+                    <Logo />
+                </Link>
+            </div>
+            <SignedOut>
+                <SignInButton>
+                    <button type="button" className="text-white px-4 py-2 cursor-pointer rounded hover:font-bold hover:underline">
+                        Sign in
+                    </button>
+                </SignInButton>
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+        </header>
     )
 }
