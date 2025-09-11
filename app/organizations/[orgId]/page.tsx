5492a56dfca5c37cbd3c74d6a8508a247966e6d8
Om Patki <om.y.patki@gmail.com>
2025-09-11 12:00:11 -0700

    second attempt at fixing image

--- app/organizations/[orgId]/page.tsx
+++ app/organizations/[orgId]/page.tsx
@@ -8,16 +8,18 @@ export default async function Organization({ params }: OrganizationPageProps) {
     
     return (
         <div className="flex justify-center gap-10 items-center my-10">
-            <Image
-            className="h-80 rounded-2xl border-2 border-[#9A3F3F] object-cover"
-            src={
-                org.image
-                ? org.image
-                : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-            }
-            alt="Organization image"
-            fill
-            />
+            <div className="relative w-full h-80">
+                <Image
+                    src={
+                    org.image
+                        ? org.image
+                        : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                    }
+                    alt="Organization image"
+                    fill
+                    className="rounded-2xl border-2 border-[#9A3F3F] object-cover"
+                />
+            </div>
             <div className="flex flex-col justify-center items-center gap-8">
                 <p className="text-2xl text-center text-[#9A3F3F] max-w-100">{org.description ? org.description : "One of the better organizations"}</p> 
                 {org.link ? <a target="_blank" className="text-2xl text-[#9A3F3F] underline hover:text-[#5A0808]" href={org.link}>{org.name}&apos;s Website</a> : ""} 
